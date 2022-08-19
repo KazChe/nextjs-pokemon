67badcecfd889a77c1116e5d17e8f7196d6a8ee8
Kaz Che <kaz.che@gmail.com>
2022-08-19 01:11:09 -0700

    SSR port of [id].js - using getServerSideProps

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,23 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../../styles/Details.module.css'
 import { useRouter } from 'next/router'
 
-export default function Details() {
-    const { query: { id } } = useRouter();
-    const [pokemon, setPokemon] = useState(null)
+export async function getServerSideProps({ params }) {
+    const resp = await fetch(`https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`)
 
-    useEffect(() => {
-        async function getPokemon() {
-            const resp = await fetch(`https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`)
-            setPokemon(await resp.json())
+    return {
+        props: {
+            pokemon: await resp.json()
         }
-        if (id) getPokemon();
-    }, [id])
-    if (!pokemon) return null
+    }
+}
 
+export default function Details({ pokemon }) {
     return (
         <div>
             <Head>
